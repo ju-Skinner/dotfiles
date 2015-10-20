dc83aec656b7098ac2b80c19d40752f779937c8f
Julian Skinner <ju-Skinner@users.noreply.github.com>
2015-10-19 21:30:37 -0400

    Update setup_dotfiles.sh
    Adding some baseline stuff for new system install.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,10 +1,28 @@
 /*
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # Generates colored output.
 function special_echo {
   echo -e '\E[0;32m'"$1\033[0m"
 }
+
+special_echo "Install Homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install wget
+brew tap homebrew/services
+brew install iterm2
+brew install vim --override-system-vim
+brew install rbenv ruby-build rbenv-default-gems rbenv-use
+brew install tmux
+brew install the_silver_searcher
+brew install python
+brew install mysql
+brew install reattach-to-user-namespace
+
+
+special_echo "Install Oh-My-Zsh"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 special_echo "Setting up $HOME/.zshrc"
 echo "source $DIR/zshrc" >> $HOME/.zshrc
