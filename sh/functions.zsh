b2b03b23f7e73bd3ea825b3974794ef41c0c705a
Julian Skinner <julian.skinner@hungrymachine.com>
2016-04-25 12:56:59 -0400

    Revert "update kill_tmux function."
    This reverts commit 1951c77081811881ebb56f2838c7ad9b8156f0ba.

--- sh/functions.zsh
+++ sh/functions.zsh
@@ -24,7 +24,7 @@ dev() { cd ~/_dev/$1; }
 _dev() { _files -W ~/_dev -/; }
 
 kill_tmux() {
-  pane_number=0
+  pane_number=3
   if [ $1 ]; then
     pane_number=$1
   fi
@@ -43,24 +43,13 @@ kill_tmux() {
 close_session() {
     pane_number=$1
     session_name=$2
-    window_count=`tmux list-windows -t $session_name | wc -l`
+    count=`tmux list-windows -t $session_name | wc -l`
 
-    for (( i=1; i <= $window_count; ++i ))
+    for (( i=1; i <= $count; ++i ))
     do
       tmux select-window -t $session_name:$i
-
-      if [ "$pane_number" = "0"]; then
-        pane_count='tmux list-panes -t $session_name | wc -l'
-        for (( pane=1; i <= $pane_count; pane++ ))
-        do
-          tmux select-pane -t $session.$pane
-          tmux send-keys -t $session_name C-c
-        done
-      else
-        tmux select-pane -t $session.$pane_number
-        tmux send-keys -t $session_name C-c
-      fi
-
+      tmux select-pane -t $session.$pane_number
+      tmux send-keys -t $session_name C-c
     done
 
     sleep 5
