b0c0746bb479edfcbcd076c4d368dd76c05f2f30
Julian Skinner <julian.skinner@hungrymachine.com>
2016-09-12 09:04:10 -0500

    Reverted back to Python 2.7
    This is in hopes to removing an error receiving from Python. ( Too many files open. )

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -41,6 +41,9 @@ echo "Linking Custom Oh My Zsh themes "
 rm -rf $ZSH_CUSTOM/themes
 ln -s $DIR/oh-my-zsh/custom/themes $ZSH_CUSTOM/themes
 
+echo "Install Powerline fonts"
+~/powerline_fonts/install.sh
+
 echo "Link Powerline config files to homedirectory config folder"
 ln -s /usr/local/lib/python2.7/site-packages/powerline/config_files $HOME/.config/powerline
 
