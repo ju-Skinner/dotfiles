b54bd40937cad18657597fa335104b2a0b7f4809
Julian Skinner <ju.skinner@gmail.com>
2020-11-11 10:19:25 -0600

    Add updates for vimrc (fzf) and zsh-prompt

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -87,9 +87,6 @@ mkdir $HOME/.rbenv
 echo "Creating Git Template Folder"
 mkdir $HOME/.git-template
 
-special_echo "Settting up $HOME/.git-template"
-echo "ref: refs/head/main" > $HOME/.git-template/HEAD
-
 echo "Configuring global gitignore file"
 git config --global core.excludesfile $DIR/gitignore
 
